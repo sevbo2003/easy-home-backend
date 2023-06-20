f86a068e83fcecf117880330f63bc92ec3502840
Sevbo <malikov@sevbo.sevbo>
2023-06-20 15:37:03 +0500

    `News` admin created

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from apps.news.models import News, Category
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'content', 'image', 'created_at', 'updated_at', 'is_featured', 'slug',)
+    list_filter = ('created_at', 'updated_at', 'is_featured', 'slug',)
+    search_fields = ('title', 'description', 'slug')
+    readonly_fields = ('created_at', 'updated_at',)
+    ordering = ('title', 'description', 'content', 'image', 'created_at', 'updated_at', 'is_featured', 'slug',)
+    fieldsets = (
+        (None, {'fields': ('title', 'description', 'content', 'image', 'is_featured', 'slug',)}),
+    )
+    prepopulated_fields = {'slug': ('title',)}
