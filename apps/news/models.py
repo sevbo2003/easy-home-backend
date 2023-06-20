f9db368a22d2c25fb54b67f572cbb0ce8b35093a
Sevbo <malikov@sevbo.sevbo>
2023-06-20 15:16:59 +0500

    `News` model created

--- apps/news/models.py
+++ apps/news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -7,3 +7,23 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class News(models.Model):
+    category = models.ManyToManyField(Category)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='news/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_featured = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(News, self).save(*args, **kwargs)
+        
