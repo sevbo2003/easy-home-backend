26074b99aa197c04436d9bb3e0bd148e0c56c177
Sevbo <malikov@sevbo.sevbo>
2023-06-22 23:49:49 +0500

    `News` admin modified

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -3,13 +3,18 @@ from apps.news.models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'content', 'image', 'created_at', 'updated_at', 'is_featured', 'slug',)
+    list_display = ('title_uz', 'description','image', 'created_at', 'updated_at', 'is_featured', 'slug',)
     list_filter = ('created_at', 'updated_at', 'is_featured', 'slug',)
     search_fields = ('title', 'description', 'slug')
     readonly_fields = ('created_at', 'updated_at',)
     ordering = ('title', 'description', 'content', 'image', 'created_at', 'updated_at', 'is_featured', 'slug',)
     fieldsets = (
-        (None, {'fields': ('title', 'description', 'content', 'image', 'is_featured', 'slug',)}),
+        ("Title", {'fields': ('title', 'title_uz', 'title_en', 'title_ru',)}),
+        ("Features", {'fields': ('image', 'is_featured')}),
+        ("Extra information", {'fields': ('category', 'description_uz', 'description_en', 'description_ru')}),
+        ("Content", {'fields': ('content_uz', 'content_en', 'content_ru')}),
+        ("Date", {'fields': ('created_at', 'updated_at', 'slug')}),
+
     )
     prepopulated_fields = {'slug': ('title',)}
 
@@ -17,7 +22,7 @@ admin.site.register(News, NewsAdmin)
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name_uz', 'name_en', 'name_ru')
     list_filter = ('name',)
     search_fields = ('name',)
     ordering = ('name',)
