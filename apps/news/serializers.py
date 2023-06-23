a196d98c5b6880639aceb45094463b32019b1075
Sevbo <malikov@sevbo.sevbo>
2023-06-23 11:55:39 +0500

    `NewsSerializers` created

--- apps/news/serializers.py
+++ apps/news/serializers.py
@@ -7,3 +7,29 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ('id', 'name_uz', 'name_en', 'name_ru')
         
+
+class NewsSerializer(serializers.ModelSerializer):
+    category = serializers.ListSerializer(child=serializers.CharField())
+    title = serializers.SerializerMethodField(method_name='get_title')
+    description = serializers.SerializerMethodField(method_name='get_description')
+    
+    class Meta:
+        model = News
+        fields = ('id', 'category', 'title', 'description', 'image', 'created_at', 'updated_at', 'is_featured', 'slug',)
+
+    def get_title(self, obj):
+        dict = {
+            'uz': obj.title_uz,
+            'en': obj.title_en,
+            'ru': obj.title_ru,
+        }
+        return dict
+    
+    def get_description(self, obj):
+        dict = {
+            'uz': obj.description_uz,
+            'en': obj.description_en,
+            'ru': obj.description_ru,
+        }
+        return dict
+    
