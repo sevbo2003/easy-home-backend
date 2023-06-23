be238db9aa3dc7510ad3ad75454a927673dc9eb8
Sevbo <malikov@sevbo.sevbo>
2023-06-23 11:56:08 +0500

    `NewsViewSets` urls created

--- apps/news/urls.py
+++ apps/news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.news.views import CategoryViewSet
+from apps.news.views import CategoryViewSet, NewsViewSet
 
 router = DefaultRouter()
 
 router.register('categories', CategoryViewSet)
+router.register('news', NewsViewSet)
 
 
 urlpatterns = [
