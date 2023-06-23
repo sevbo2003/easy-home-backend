b2935f27c33edd919c69c26c29a371984c30ec77
Sevbo <malikov@sevbo.sevbo>
2023-06-23 11:55:56 +0500

    `NewsViewSets` created

--- apps/news/views.py
+++ apps/news/views.py
@@ -3,11 +3,17 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 from apps.news.models import News, Category
-from apps.news.serializers import CategorySerializer
+from apps.news.serializers import CategorySerializer, NewsSerializer
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    http_method_names = ['get', 'head', 'options']
+
+
+class NewsViewSet(viewsets.ModelViewSet):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
     http_method_names = ['get', 'head', 'options']
     
