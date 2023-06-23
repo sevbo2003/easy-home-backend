9e434486f4e477354ed5fe37c6614687c03fb77a
Sevbo <malikov@sevbo.sevbo>
2023-06-23 11:46:30 +0500

    `CategoryViewSets` created

--- apps/news/views.py
+++ apps/news/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from apps.news.models import News, Category
+from apps.news.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    http_method_names = ['get', 'head', 'options']
+    
