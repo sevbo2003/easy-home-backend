baeb5f361c8da34e6aec8e8248a72e4556def769
Sevbo <malikov@sevbo.sevbo>
2023-06-27 02:28:41 +0500

    `send_news` task created

---
+++ apps/newsletter/tasks.py
@@ -0,0 +1,14 @@
+from celery import shared_task
+from core.eskiz import eskiz
+# from apps.stroy.models import Newsletter
+
+
+@shared_task
+def send_news(numbers,message):
+    try:
+        for i in numbers:
+            eskiz.send_sms(str(i)[1:], message, from_whom='4546')
+        return {"status": "success", "message": "Xabar muvaffaqiyatli yuborildi"}
+    except Exception as e:
+        return {"status": "error", "message": e}
+    
