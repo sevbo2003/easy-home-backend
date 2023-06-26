2cc6cb9aa72865946eb0b4d871c080a0491a10c5
Sevbo <malikov@sevbo.sevbo>
2023-06-27 00:40:01 +0500

    `verify_token` function created

--- apps/contact/utils.py
+++ apps/contact/utils.py
@@ -11,3 +11,16 @@ def generate_token(phone_number):
     phone_token.expires_at = timezone.now() + timezone.timedelta(minutes=3)
     phone_token.save()
     return token
+
+
+def verify_token(phone_number, token):
+    try:
+        phone_token = PhoneToken.objects.filter(phone_number=phone_number).last()
+        if phone_token.token == token and phone_token.expires_at > timezone.now():
+            phone_token.is_verified = True
+            phone_token.save()
+            return True
+        return False
+    except PhoneToken.DoesNotExist:
+        return False
+    
