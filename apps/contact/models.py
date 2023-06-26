0bfd678c1c34d799df5a577ac2789feecb78254d
Sevbo <malikov@sevbo.sevbo>
2023-06-27 00:13:34 +0500

    `Contact` model created

--- apps/contact/models.py
+++ apps/contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from apps.contact.validators import validate_uzb_phone_number
 
-# Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=100, validators=[validate_uzb_phone_number])
+    email = models.EmailField()
+    message = models.CharField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'Contacts'
+    
