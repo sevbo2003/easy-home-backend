76ffb6f5c91c5520c5fcbbdfe6e9f45ee3571b9f
Sevbo <malikov@sevbo.sevbo>
2023-06-27 01:50:15 +0500

    `Newsletter` model created

--- apps/newsletter/models.py
+++ apps/newsletter/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from apps.contact.validators import validate_uzb_phone_number
 
-# Create your models here.
+
+class Newsletter(models.Model):
+    phone_number = models.CharField(max_length=20, validators=[validate_uzb_phone_number])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.phone_number
+
+    class Meta:
+        verbose_name = _('Foydalanuvchi')
+        verbose_name_plural = _('Foydalanuvchilar')
+        ordering = ('-created_at',)
