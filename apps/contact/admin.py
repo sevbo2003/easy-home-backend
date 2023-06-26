9b7889e890cc0585bcd937861520176db8659314
Sevbo <malikov@sevbo.sevbo>
2023-06-27 00:22:47 +0500

    `ContactAdmin` created

--- apps/contact/admin.py
+++ apps/contact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from apps.contact.models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone_number', 'email', 'message', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'phone_number', 'email')
+    readonly_fields = ('name', 'phone_number', 'email', 'message', 'created_at')
+    fieldsets = (
+        ('Contact', {
+            'fields': ('name', 'phone_number', 'email', 'message', 'created_at')
+        }),
+    )
+    
