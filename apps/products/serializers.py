018e810c0185aeeff294cc2a7325b4b4b5c9f0bb
Sevbo <malikov@sevbo.sevbo>
2023-06-28 14:31:19 +0500

    `CategorySerializer` created for `Products`

---
+++ apps/products/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from apps.products.models import Category, Product, Parameter, ProductSliderImage, Document
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(method_name='get_name')
+
+    class Meta:
+        model = Category
+        ref_name = 'ProductsCategory' 
+        fields = ['id', 'name', 'slug']
+    
+    def get_name(self, obj):
+        dict = {
+            'uz': obj.name_uz,
+            'en': obj.name_en,
+            'ru': obj.name_ru,
+        }
+        return dict
+    
