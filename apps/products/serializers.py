951dc6ee7f1feed4eb8c5dc78ca7eb1060d2bb94
Sevbo <malikov@sevbo.sevbo>
2023-06-28 14:54:02 +0500

    `ProductSerializer` created

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -39,6 +39,9 @@ class ProductSliderImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductSliderImage
         fields = ['image']
+    
+    def to_representation(self, instance):
+        return super().to_representation(instance).get('image')
 
 
 class DocumentSerializer(serializers.ModelSerializer):
@@ -55,4 +58,40 @@ class DocumentSerializer(serializers.ModelSerializer):
             'ru': obj.name_ru,
         }
         return dict
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField(method_name='get_title')
+    description = serializers.SerializerMethodField(method_name='get_description')
+    price_uzs = serializers.SerializerMethodField(method_name='get_price_uzs')
+    parameters = ParameterSerializer(many=True)
+    slider_images = ProductSliderImageSerializer(many=True)
+    documents = DocumentSerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'image', 'slug', 'category', 'price_uzs', 'is_featured', 'is_active', 'parameters', 'slider_images', 'documents']
+    
+    def get_title(self, obj):
+        dict = {
+            'uz': obj.title_uz,
+            'en': obj.title_en,
+            'ru': obj.title_ru,
+        }
+        return dict
+    
+    def get_description(self, obj):
+        dict = {
+            'uz': obj.description_uz,
+            'en': obj.description_en,
+            'ru': obj.description_ru,
+        }
+        return dict
+    
+    def get_price_uzs(self, obj):
+        return [{
+            "currency": "uzs",
+            "amount": obj.price_uzs
+        }]
     
