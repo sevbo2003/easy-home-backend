bd955717a2ed4b56feae8bfc67d79b05a169179d
Sevbo <malikov@sevbo.sevbo>
2023-06-28 14:54:24 +0500

    `ProductViewSets` urls created

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.products.views import CategoryViewSet
+from apps.products.views import CategoryViewSet, ProductViewSet
 
 
 router = DefaultRouter()
 
 router.register('categories', CategoryViewSet, basename='categories')
+router.register('products', ProductViewSet, basename='products')
 
 urlpatterns = [
     path('', include(router.urls)),
