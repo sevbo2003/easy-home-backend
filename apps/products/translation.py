4b85c059620c2499042149484c2ff598a37966b8
Sevbo <malikov@sevbo.sevbo>
2023-06-28 14:23:43 +0500

    `translation` created for `Product` model

--- apps/products/translation.py
+++ apps/products/translation.py
@@ -1,8 +1,26 @@
 from modeltranslation.translator import translator, TranslationOptions
-from apps.products.models import Category
+from apps.products.models import Category, Product, Parameter, Document
 
 
 class CategoryTranslationOptions(TranslationOptions):
     fields = ('name',)
 
 translator.register(Category, CategoryTranslationOptions)
+
+
+class ProductTranslationOptions(TranslationOptions):
+    fields = ('title', 'description',)
+
+translator.register(Product, ProductTranslationOptions)
+
+
+class ParameterTranslationOptions(TranslationOptions):
+    fields = ('name', 'value',)
+
+translator.register(Parameter, ParameterTranslationOptions)
+
+
+class DocumentTranslationOptions(TranslationOptions):
+    fields = ('name',)
+
+translator.register(Document, DocumentTranslationOptions)
