05225763f12707950308cc7820ed6e792c373e26
Sevbo <malikov@sevbo.sevbo>
2023-06-29 00:08:27 +0500

    `Service` model created

--- apps/service/models.py
+++ apps/service/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from apps.service.managers import ServiceManager
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='service')
+    content = models.TextField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    objects = ServiceManager()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
+        ordering = ('-created_at',)
+    
