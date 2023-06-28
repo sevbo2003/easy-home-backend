46f2f9170406a068f30513269d619660fec3fb36
Sevbo <malikov@sevbo.sevbo>
2023-06-28 13:35:29 +0500

    `Category` model created for `Product`

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
