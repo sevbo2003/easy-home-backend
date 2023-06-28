304135db98ea2660da34e15b4b3d88b3d01e91d1
Sevbo <malikov@sevbo.sevbo>
2023-06-28 14:31:37 +0500

    `CategoryViewSets` created

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from apps.products.models import Category, Product, Parameter, ProductSliderImage, Document
+from apps.products.serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    http_method_names = ['get', 'head', 'options']
+    lookup_field = 'slug'
+    
