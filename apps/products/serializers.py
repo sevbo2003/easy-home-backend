91e429606ff89ab84d22974adf527a43ae6e27b5
Sevbo <malikov@sevbo.sevbo>
2023-06-28 15:05:49 +0500

    `ProductRetrieveSerializer` created

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from apps.products.models import Category, Product, Parameter, ProductSliderImage, Document
+from django.conf import settings
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -60,7 +61,7 @@ class DocumentSerializer(serializers.ModelSerializer):
         return dict
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductRetrieveSerializer(serializers.ModelSerializer):
     title = serializers.SerializerMethodField(method_name='get_title')
     description = serializers.SerializerMethodField(method_name='get_description')
     price_uzs = serializers.SerializerMethodField(method_name='get_price_uzs')
@@ -95,3 +96,31 @@ class ProductSerializer(serializers.ModelSerializer):
             "amount": obj.price_uzs
         }]
     
+
+class ProductSerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField(method_name='get_title')
+    price_uzs = serializers.SerializerMethodField(method_name='get_price_uzs')
+    image = serializers.SerializerMethodField(method_name='get_image')
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'image', 'slug', 'price_uzs', 'is_featured', 'is_active']
+    
+    def get_title(self, obj):
+        dict = {
+            'uz': obj.title_uz,
+            'en': obj.title_en,
+            'ru': obj.title_ru,
+        }
+        return dict
+    
+    def get_price_uzs(self, obj):
+        return [{
+            "currency": "uzs",
+            "amount": obj.price_uzs
+        }]
+    
+    def get_image(self, obj):
+        if obj.image.url[0] == '/':
+            return settings.MAIN_DOMAIN + obj.image.url
+        return obj.image
