3b1a2072b77fe4170cc289048ab7de61ee790b43
Sevbo <malikov@sevbo.sevbo>
2023-06-29 00:13:37 +0500

    `KeyFeaturesTranslation` created

--- apps/service/translation.py
+++ apps/service/translation.py
@@ -1,8 +1,14 @@
 from modeltranslation.translator import translator, TranslationOptions
-from apps.service.models import Service
+from apps.service.models import Service, KeyFeatures
 
 
 class ServiceTranslationOptions(TranslationOptions):
     fields = ('name', 'content',)
 
 translator.register(Service, ServiceTranslationOptions)
+
+
+class KeyFeaturesTranslationOptions(TranslationOptions):
+    fields = ('title',)
+
+translator.register(KeyFeatures, KeyFeaturesTranslationOptions)
