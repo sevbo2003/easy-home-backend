0d0861ebe0e453726f413309ac5081bc2de3493d
Sevbo <malikov@sevbo.sevbo>
2023-06-29 00:29:22 +0500

    `ServiceViewSet` created

--- apps/service/views.py
+++ apps/service/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from apps.service.models import Service
+from apps.service.serializers import ServiceSerializer, ServiceRetrieveSerializer
 
-# Create your views here.
+
+class ServiceViewSet(ModelViewSet):
+    queryset = Service.objects.filter(is_active=True)
+    serializer_class = ServiceSerializer
+    http_method_names = ['get', 'head', 'options']
+    lookup_field = 'slug'
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return ServiceRetrieveSerializer
+        return ServiceSerializer
+    
