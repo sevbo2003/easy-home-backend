ac4c77dbc171d60a1442a3fe9a93ffc1c1906f6f
Sevbo <malikov@sevbo.sevbo>
2023-06-28 15:15:11 +0500

    `search_filter` filter implemented

--- apps/products/filters.py
+++ apps/products/filters.py
@@ -4,7 +4,12 @@ from apps.products.models import Product
 
 class ProductFilter(filters.FilterSet):
     price_range = filters.RangeFilter(field_name='price_uzs')
+    search = filters.CharFilter(method='search_filter')
 
     class Meta:
         model = Product
-        fields = ['price_range']
+        fields = ['price_range', 'search']
+
+    def search_filter(self, queryset, name, value):
+        return queryset.filter(title_uz__icontains=value) | queryset.filter(title_en__icontains=value) | queryset.filter(title_ru__icontains=value)
+    
