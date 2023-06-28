61cedd85726c94009c2c3c1de888c3c38ee08df3
Sevbo <malikov@sevbo.sevbo>
2023-06-29 00:28:18 +0500

    `ServiceSerializer` created

--- apps/service/serializers.py
+++ apps/service/serializers.py
@@ -26,3 +26,48 @@ class KeyFeaturesSerializer(serializers.ModelSerializer):
         }
         return dict
     
+
+class ServiceSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(method_name='get_name')
+    key_features = KeyFeaturesSerializer(many=True)
+
+    class Meta:
+        model = Service
+        fields = ('id', 'name', 'key_features', 'image', 'slug')
+
+    def get_name(self, obj):
+        dict = {
+            'uz': obj.name_uz,
+            'en': obj.name_en,
+            'ru': obj.name_ru,
+        }
+        return dict
+
+
+class ServiceRetrieveSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(method_name='get_name')
+    images = ServiceImageSerializer(many=True)
+    key_features = KeyFeaturesSerializer(many=True)
+    content = serializers.SerializerMethodField(method_name='get_content')
+
+    class Meta:
+        model = Service
+        fields = ('id', 'name', 'key_features', 'image', 'images', 'content', 'slug')
+
+    def get_name(self, obj):
+        dict = {
+            'uz': obj.name_uz,
+            'en': obj.name_en,
+            'ru': obj.name_ru,
+        }
+        return dict
+    
+
+    def get_content(self, obj):
+        dict = {
+            'uz': obj.content_uz,
+            'en': obj.content_en,
+            'ru': obj.content_ru,
+        }
+        return dict
+    
