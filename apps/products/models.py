3360735ba19fa2c4acd8d35e35b2c38de6937293
Sevbo <malikov@sevbo.sevbo>
2023-06-28 14:23:17 +0500

    `Product` model created

--- apps/products/models.py
+++ apps/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from apps.products.managers import ProductManager
 
 
 class Category(models.Model):
@@ -12,3 +13,64 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price_uzs = models.PositiveIntegerField()
+    description = models.CharField(max_length=1200)
+    image = models.ImageField(upload_to='product-main-images')
+    slug = models.SlugField(max_length=255, unique=True)
+    is_featured = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    objects = ProductManager()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title_uz
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+        ordering = ('-created_at',)
+    
+
+class Parameter(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    value = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Parameter'
+        verbose_name_plural = 'Parameters'
+    
+
+class ProductSliderImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product-slider-images')
+
+    def __str__(self) -> str:
+        return self.product.title
+    
+    class Meta:
+        verbose_name = 'ProductSliderImage'
+        verbose_name_plural = 'ProductSliderImages'
+
+
+class Document(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='product-documents')
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Document'
+        verbose_name_plural = 'Documents'
+        
