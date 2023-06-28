158da26a70716d545d4dcb75e203e13ae7d76a82
Sevbo <malikov@sevbo.sevbo>
2023-06-29 00:09:41 +0500

    `ServiceImage` model created

--- apps/service/models.py
+++ apps/service/models.py
@@ -19,4 +19,8 @@ class Service(models.Model):
         verbose_name = 'Service'
         verbose_name_plural = 'Services'
         ordering = ('-created_at',)
-    
+
+
+class ServiceImage(models.Model):
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='service')
